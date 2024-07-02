c897799d9c389da218d8159106be5aaa5bc07702
Chiara Monforte <75482817+MOchiara@users.noreply.github.com>
2024-07-02 15:03:58 +0200

    Check (I feel like we are in a chicken or egg situation haha)

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -6,18 +6,18 @@ def test_plots():
     ds = fetchers.load_sample_dataset()
     tools.plot_basic_vars(ds, end_prof=int(ds.PROFILE_NUMBER.max()))
 
-
+'''
 def test_up_down_bias():
     ds = fetchers.load_sample_dataset()
     fig, ax = plt.subplots()
     tools.updown_bias(ds, var='PSAL', v_res=1)
-
+'''
 
 def test_chl():
     ds = fetchers.load_sample_dataset()
     tools.chl_first_check(ds)
 
-
+'''
 def test_quench_sequence():
     ds = fetchers.load_sample_dataset()
     if not "TIME" in ds.indexes.keys():
@@ -25,3 +25,4 @@ def test_quench_sequence():
     tools.plot_section_with_srss(ax, ds, sel_var='CHLA',start_time = '2023-09-06', end_time = '2023-09-10', ylim=35)
     dayT, nightT = tools.day_night_avg(ds, sel_var='TEMP',start_time = '2023-09-06', end_time = '2023-09-10')
     tools.plot_daynight_avg( dayT, nightT, ax[0],sel_day='2023-09-08', xlabel='Temperature [C]')
+'''
