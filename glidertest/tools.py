df149dfe4f11285326a56a313cc49d76d5caedf9
ChiaraMonforte <chiara.monforte@ad.cen.uni-hamburg.de>
2024-07-15 15:35:24 +0200

    Improved function

--- glidertest/tools.py
+++ glidertest/tools.py
@@ -488,11 +488,10 @@ def plot_section_with_srss(ds, ax, sel_var='TEMP',start_time = '2023-09-06', end
     plt.colorbar(c, label=f'{sel_var} [{ds[sel_var].units}]')
     return ax 
  
-def check_temporal_drift(ds: pd.DataFrame, var: str,ax: plt.Axes = None, **kw: dict,)-> tuple({plt.Figure, plt.Axes}):
+def check_temporal_drift(ds: xr.Dataset, var: str,ax: plt.Axes = None, **kw: dict,)-> tuple({plt.Figure, plt.Axes}):
     if ax is None:
         fig, ax = plt.subplots(1, 2, figsize=(14, 6))
     else:
-        ax = ax
         fig = plt.gcf()
         
     ax[0].scatter(mdates.date2num(ds.TIME),ds[var], s=10)
