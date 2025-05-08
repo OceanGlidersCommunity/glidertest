cb35d8d7b72fc49627bb85a1b3a8156465cc79dc
MOchiara <monforte.chiara.mc@gmail.com>
2025-05-08 12:20:49 +0200

    [FEAT] Create summary sheet .rst and .md

--- tests/test_mission_summary.py
+++ tests/test_mission_summary.py
@@ -1,23 +1,30 @@
 from glidertest import fetchers, summary_sheet
-import pandas as pd
 import matplotlib
+from pathlib import Path
 
 matplotlib.use('agg')  # use agg backend to prevent creating plot windows during tests
 
+
+library_dir = Path(__file__).parent.parent.absolute()
+example_dir = library_dir / 'tests/example-summarysheet/'
 def test_qc_checks():
     ds = fetchers.load_sample_dataset()
     gr, spike, flat, err_mean,err_range = summary_sheet.qc_checks(ds, var='PSAL')
 def test_tableqc():
     ds = fetchers.load_sample_dataset()
-    df_test = pd.DataFrame({'': ['Global range', 'Spike test', 'Flat test', 'Hysteresis (mean)', 'Hysteresis (range)', 'Drift'],
-                        'Temperature': ['✓', '✓', '✓', '✓', '✓', '✓'],
-                        'Salinity': ['✓', '✓', '✓', '✓', '✓', '✓']})
-    tableT = summary_sheet.fill_tableqc(df_test,ds, var='TEMP')
+    strgr = ['Global range', '✓', '✓', '✓', '✓']
+    strst = ['Spike test', '✓', '✓', '✓', '✓']
+    strft = ['Flat test', '✓', '✓', '✓', '✓']
+    strhy = ['Hysteresis', '✓', '✓', '✓', '✓']
+    strdr = ['Drift', '✓', '✓', '✓', '✓']
+    summary_sheet.fill_str(strgr, strst, strft, strhy, strdr,ds, var='TEMP')
 def test_phrase_duration_check():
     ds = fetchers.load_sample_dataset()
     summary_sheet.phrase_numberprof_check(ds)
     summary_sheet.phrase_duration_check(ds)
 def test_summary_plot():
     ds = fetchers.load_sample_dataset()
-    summary_sheet.summary_plot(ds, test=False)
-    summary_sheet.summary_plot_template(ds,var='PSAL')
+    summary_sheet.create_docfile(ds, path=f'{example_dir}/ex_rst.rst')
+    summary_sheet.rst_to_md(input_rst_path = f'{example_dir}/ex_rst.rst', output_md_path = f'{example_dir}/ex_md.md')
+    summary_sheet.mission_report(ds, example_dir)
+    summary_sheet.template_docfile(ds, path=f'{example_dir}/template.rst')
