d373b46585edf480eab76f882e124c97505e7880
MOchiara <monforte.chiara.mc@gmail.com>
2025-05-08 12:48:32 +0200

    Bad reference to folder path

--- tests/test_mission_summary.py
+++ tests/test_mission_summary.py
@@ -4,9 +4,6 @@ from pathlib import Path
 
 matplotlib.use('agg')  # use agg backend to prevent creating plot windows during tests
 
-
-library_dir = Path(__file__).parent.parent.absolute()
-example_dir = f'{library_dir}/tests/example-summarysheet/'
 def test_qc_checks():
     ds = fetchers.load_sample_dataset()
     gr, spike, flat, err_mean,err_range = summary_sheet.qc_checks(ds, var='PSAL')
@@ -23,6 +20,8 @@ def test_phrase_duration_check():
     summary_sheet.phrase_numberprof_check(ds)
     summary_sheet.phrase_duration_check(ds)
 def test_summary_plot():
+    library_dir = Path(__file__).parent.parent.absolute()
+    example_dir = f'{library_dir}/tests/example-summarysheet/'
     ds = fetchers.load_sample_dataset()
     summary_sheet.create_docfile(ds, path=f'{example_dir}/ex_rst.rst')
     summary_sheet.rst_to_md(input_rst_path = f'{example_dir}/ex_rst.rst', output_md_path = f'{example_dir}/ex_md.md')
