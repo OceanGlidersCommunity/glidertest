863ceadac9f993a0360b3df08c3d1176e46be51c
Chiara Monforte <monforte.chiara.mc@gmail.com>
2025-05-27 16:47:04 +0200

    Issues with the small change to plots.process_optics_assess

--- tests/test_plots.py
+++ tests/test_plots.py
@@ -36,7 +36,7 @@ def test_chl(var1='CHLA', var2='BBP700'):
     ds = fetchers.load_sample_dataset()
     fig, ax = plots.process_optics_assess(ds, var=var1)
     assert ax.get_ylabel() == f'{utilities.plotting_labels(var1)} ({utilities.plotting_units(ds,var1)})'
-    ax = plots.process_optics_assess(ds, var=var2)
+    fig, ax = plots.process_optics_assess(ds, var=var2)
     assert ax.get_ylabel() == f'{utilities.plotting_labels(var2)} ({utilities.plotting_units(ds,var2)})'
 
 
